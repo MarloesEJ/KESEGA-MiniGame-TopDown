736c1e28267f9aa60c053f157a964f0265aec87e
marloesEJ <158572639+MarloesEJ@users.noreply.github.com>
2026-03-28 01:31:08 +0100

    add shaking effect to the tile before it falls

--- Assets/scripts/CrumblingTile.cs
+++ Assets/scripts/CrumblingTile.cs
@@ -2,11 +2,39 @@ using UnityEngine;
 
 public class CrumblingTile : MonoBehaviour
 {
-    public float fallDelay = 0.8f;
+    [Header("Timings")]
+    public float fallDelay = 3.0f;
+    public float shakeDelay = 0.4f;
+
+    [Header("Shake Settings")]
+    public float shakeAmount = 0.05f;
+    public float shakeSpeed = 20f;
+
+    [Header("References")]
     public GameObject tileVisual;
+
     private bool isStappedOn = false;
+    private bool isShaking = false;
     private bool playerOnTile = false;
     private bool hasFallen = false;
+
+    private Vector3 originalLocalPos;
+
+    void Start()
+    {
+        originalLocalPos = tileVisual.transform.localPosition;
+    }
+
+    void Update()
+    {
+        if (isShaking)
+        {
+            float shakeX = Mathf.Sin(Time.time * shakeSpeed)* shakeAmount;
+            float shakeZ = Mathf.Cos(Time.time * shakeSpeed)* shakeAmount;
+
+            tileVisual.transform.localPosition = originalLocalPos + new Vector3(shakeX, 0, shakeZ);
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -17,6 +45,7 @@ public class CrumblingTile : MonoBehaviour
             if (!isStappedOn)
             {
                 isStappedOn = true;
+                Invoke("StartShaking", shakeDelay);
                 Invoke("DestroyTile", fallDelay);
             }
 
@@ -35,10 +64,19 @@ public class CrumblingTile : MonoBehaviour
         }
     }
 
+    void StartShaking()
+    {
+        isShaking = true;
+    }
+
     void DestroyTile()
     {
+        isShaking = false;
         hasFallen = true;
+
+        tileVisual.transform.localPosition = originalLocalPos;
         tileVisual.SetActive(false);
+
         if (playerOnTile)
         {
             ReloadLevel();
